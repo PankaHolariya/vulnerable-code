fc5b022febf04ab2be9b78a3bc29c46fd6647cca
PankaHolariya <87966660+PankaHolariya@users.noreply.github.com>
2025-02-19 15:29:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(helmet()); // Adds security headers
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello Secure World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Secure app listening at http://localhost:${port}`);
 });
